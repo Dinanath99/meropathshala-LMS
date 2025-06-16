0fa1f9e642137f0293ec8889effbb67058fd582d
Dinanath <dkmdkm26@gmail.com>
2025-06-17 00:36:17 +0545

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <div>
+      <Button className="m-2" variant="default">
+        Button
+      </Button>
+    </div>
+  );
+};
+
+export default App;
