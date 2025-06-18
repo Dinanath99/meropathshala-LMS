caf99ccb21d41f124b6cdd665c71ef04cd2aacef
Dinanath <dkmdkm26@gmail.com>
2025-06-18 16:52:54 +0545

    backend server setup & db connection successfully added

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error occured", error);
+  });
